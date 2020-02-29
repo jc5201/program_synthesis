44952c279fed120c337f191ef61efb9dd3af75f2
NGD <jch5201@naver.com>
2020-03-01 04:33:38 +0900

    in-progress: implement uast to python ast

--- test_uast_to_ast.py
+++ test_uast_to_ast.py
@@ -1,4 +1,6 @@
 import json
+import uast_to_ast
+import astunparse
 
 with open('naps_example_trainB.jsonl', 'r') as json_file:
     json_list = list(json_file)
@@ -8,3 +10,8 @@ for json_str in json_list:
     data.append(json.loads(json_str))
 uast1 = data[0]['code_tree']
 uast2 = data[1]['code_tree']
+
+past1 = uast_to_ast.convert_program(uast1)
+pcode = astunparse.unparse(past1)
+print(pcode)
+
--- uast_to_ast.py
+++ uast_to_ast.py
@@ -1,14 +1,15 @@
 import ast
 import copy
+import re
 
 
 def uast_to_python_ast(uast) -> ast.AST:
-    tree = ast.AST()
+    tree = ast.Module()
     tree.body = convert_program(uast)
     return tree
 
 
-def convert_program(program) -> list[ast.AST]:
+def convert_program(program) -> list:
     types = program['types']
     funcs = program['funcs']
     body = []
@@ -43,21 +44,100 @@ def convert_func(func) -> ast.AST:
         NotImplemented
 
     stmt_list = [convert_stmt(stmt) for stmt in body]
-    return ast.FunctionDef(name, convert_args(arguments), stmt_list)
+    return ast.FunctionDef(name, convert_args(arguments), stmt_list, [], None)
 
 
 def convert_args(args) -> ast.arguments:
     args_list = []
     for arg in args:
         assert arg[0] == 'var'
-        args_list.append(ast.arg(convert_type(arg[1]), arg[2]))
+        args_list.append(ast.arg(convert_type(arg[1]), convert_var(arg)))
     return ast.arguments(args=args_list, varargs=None, kwarg=None, defaults=[])
 
 
+def convert_type(t:str) -> type:
+    if t.startswith('<'):
+        return dict
+    elif '*' in t:
+        return list[convert_type(t[:-1])]
+    elif '%' in t:
+        return set
+    elif '#' in t:
+        raise Exception("type __global__ triggered")
+    else:
+        type_map = {'bool': bool, 'char': chr, 'int': int, 'real': float}
+        return type_map[t]
+
+
 def convert_stmt(stmt) -> ast.AST:
-    NotImplemented
+    if stmt[0] == 'if':
+        _, _, condition, body_if, body_else = stmt
+        if_test = convert_expr(condition)
+        if_body = [convert_stmt(line) for line in body_if]
+        else_body = [convert_stmt(line) for line in body_else]
+        node = ast.If(if_test, if_body, else_body)
+        return node
+    elif stmt[0] == 'foreach':
+        _, _, var, collection, body = stmt
+        target = convert_var(var)
+        iter = convert_expr(collection)
+        for_body = [convert_stmt(line) for line in body]
+        node = ast.For(target, iter, for_body, [])
+        return node
+    elif stmt[0] == 'while':
+        _, _, condition, body, body_inc = stmt
+        while_test = convert_expr(condition)
+        while_body = [convert_stmt(line) for line in body + body_inc]
+        node = ast.While(while_test, while_body, [])
+        return node
+    elif stmt[0] == 'break':
+        return ast.Break
+    elif stmt[0] == 'continue':
+        return ast.Continue
+    elif stmt[0] == 'return':
+        _, _, expr = stmt
+        return ast.Return(convert_expr(expr))
+    elif stmt[0] == 'noop':
+        return ast.Pass
+    else:
+        return convert_expr(stmt)
 
 
-def convert_type(type) -> type:
-    NotImplemented
+def convert_expr(expr) -> ast.AST:
+    if expr[0] == 'assign':
+        _, _, lhs, rhs = expr
+        return ast.Assign([convert_expr(lhs)], convert_expr(rhs))
+    if expr[0] == 'var':
+        return convert_var(expr)
+    if expr[0] == 'field':
+        _, _, exp, field = expr
+        return ast.Attribute(convert_expr(exp), field, ast.Load())
+    if expr[0] == 'val':
+        _, typ, value = expr
+        if typ == 'bool':
+            if value == 'true':
+                return ast.NameConstant(True)
+            else:
+                return ast.NameConstant(False)
+        elif typ == 'str':
+            return ast.Str(value)
+        else:
+            return ast.Num(value)
+    if expr[0] == 'invoke':
+        _, _, func_name, args = expr
+        args = [convert_expr(arg) for arg in args]
+        func = func_name # TODO
+        ast.Call(ast.Name(func, ast.Load()), args, [])
+    if expr[0] == '?:':
+        _, _, condition, expr_if, expr_else = expr
+        test = convert_expr(condition)
+        return ast.IfExp(test, convert_expr(expr_if), convert_expr(expr_else))
+    if expr[0] == 'cast':
+        _, typ, exp = expr
+        func = str(convert_type(typ)) # TODO: function name for each type
+        ast.Call(ast.Name(func), [convert_expr(exp)], [])
+
+
+def convert_var(var) -> ast.Name:
+    return ast.Name(var[2], ast.Load())
 
